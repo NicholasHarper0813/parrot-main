68b824c5859a03e0638f9279d47bb95ec86a8e0f
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:21:53 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express     = require('express');
 const router      = express.Router();
-
 const auth        = require('../middlewares/auth');
 const noauth      = require('../middlewares/noauth');
 const controller  = require( '../controllers/auth-controller' );
