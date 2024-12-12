ce952f3d757f714396a20851aaa7257f43061ea2
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:22:45 -0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,8 @@
 const express     = require( 'express' );
 const router      = express.Router();
-
 const auth        = require( '../middlewares/auth' );
 const controller  = require( '../controllers/user-controller' );
 
-// AJAX REQUESTS
 router.post( '/follow',    auth, controller.follow );
 router.post( '/unfollow',  auth, controller.unfollow );
 
