e1a632c906f052af4ecc98299846b5d93f0b7742
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:24:27 -0800

    Update user-service.js

--- services/user-service.js
+++ services/user-service.js
@@ -8,9 +8,9 @@ exports.slugfy = ( string ) => {
     return string.toString()
         .replace(/\s+/g, '') // Replace spaces with non spaces
         .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-        .replace(/&/g, '') // Replace & with 'and'
         .replace(/[^\w\-]+/g, '') // Remove all non-word characters
         .replace(/\-\-+/g, '-') // Replace multiple - with single -
+        .replace(/&/g, '') // Replace & with 'and'
         .replace(/^-+/, '') // Trim - from start of text
         .replace(/-+$/, ''); // Trim - from end of text
 };
