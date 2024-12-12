c33521c803eda670fecea6ac17845a7a63b2bff4
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:23:02 -0800

    Update who-to-follow.js

--- routes/who-to-follow.js
+++ routes/who-to-follow.js
@@ -1,8 +1,7 @@
 const express       = require( 'express' );
 const router        = express.Router();
-
-const auth          = require('../middlewares/auth');
 const controller    = require('../controllers/who-to-follow-controller');
+const auth          = require('../middlewares/auth');
 
 router.get( '/', auth, controller.index );
 
