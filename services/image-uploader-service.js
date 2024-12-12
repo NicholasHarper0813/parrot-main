1edd1af4fd0a3cfa2b3f6bc2b9f7f4749296a2b9
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:23:47 -0800

    Update image-uploader-service.js

--- services/image-uploader-service.js
+++ services/image-uploader-service.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 
 exports.uploadFromBinary = ( data, filename, destination ) => {
     const imageData = data ? data.replace('data:image/png;base64,', '') : '';
-    const buffer    = Buffer.from( imageData, 'base64' );
     const filepath  = destination + filename;
+    const buffer    = Buffer.from( imageData, 'base64' );
       
-    if ( imageData.length ) {
+    if ( imageData.length ) 
+    {
         fs.writeFileSync( filepath, buffer);
         return true;
     }
-
+    
     return false;
 };
