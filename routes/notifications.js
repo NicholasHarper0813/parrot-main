b9042d50fc4b5e5546f7deff8de61cc6f9127e30
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:22:05 -0800

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,9 +1,7 @@
 const express       = require('express');
 const router        = express.Router();
-
 const auth          = require('../middlewares/auth');
 const controller    = require( '../controllers/notifications-controller' );
 
 router.get( '/', auth, controller.index );
-
 module.exports = router;
