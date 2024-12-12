2c9f26e85f521aa34b4d4eaa1e9ec59455306b22
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 17:48:13 -0800

    Update user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,17 +3,8 @@
 const userRepository            = require( '../repositories/user-repository' );
 const notificationRepository    = require( '../repositories/notification-repository' );
 
-// Apenas via Ajax
 exports.follow = async ( req, res, next ) => {
     try {
-        /*
-        * TODO 
-        * ver como fazer transaction e como Relacionar os models
-        * fazer query antes para ver se usuario ja seguiu
-        * este usuario hoje e em caso positivo nao criar notificação !
-        * Este caso se aplica qnd um usuario segue e nao segue mais o mesmo usuario no mesmo dia
-        * pensar e "dia" é suficiente ou talvez "semana" para evitar spam na base de notificação e no tela do usuario
-        */
         const myId      = req.session.user.id;
         const friendId  = req.body.user_id;
 
@@ -24,17 +15,15 @@ exports.follow = async ( req, res, next ) => {
 
         res.json({ status: true });
     } 
-    catch ( e ) {
-        console.log( 'ERROR FOLLOW ################' );
+    catch ( e ) 
+    {
         console.log( e.message );
         return res.status(500).json({ status: true });
     }
 
 };
 
-// Apenas via Ajax
 exports.unfollow = async ( req, res, next ) => {
-    // TODO ver como fazer transaction e como Relacionar os models
     try {
         const myId      = req.session.user.id;
         const friendId  = req.body.user_id;
@@ -45,8 +34,8 @@ exports.unfollow = async ( req, res, next ) => {
 
         res.json({ status: true });
     } 
-    catch ( e ) {
-        console.log( 'ERROR FOLLOW ################' );
+    catch ( e ) 
+    {
         console.log( e.message );
         return res.status(500).json({ status: true });
     }
