65c0a433eaabbe3d60f179804e7d4f0dddbde171
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:22:14 -0800

    Update password-recover.js

--- routes/password-recover.js
+++ routes/password-recover.js
@@ -1,6 +1,5 @@
 const express       = require( 'express' );
 const router        = express.Router();
-
 const noauth        = require( '../middlewares/noauth' );
 const controller    = require( '../controllers/auth-controller' );
 
