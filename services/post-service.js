ef97cf08719cce345bd283229fd6eb7f26904d9a
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:24:03 -0800

    Update post-service.js

--- services/post-service.js
+++ services/post-service.js
@@ -3,8 +3,8 @@ const Entities = require('html-entities').XmlEntities;
 
 exports.parseBody = ( content ) => {
     const entities  = new Entities();
-    
     let post = content.substring(0, 240);
+    
     post = entities.encode( post );
     post = post.replace(/(?:\r\n|\r|\n)/g, '<br>');
     
