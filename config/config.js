fd004ec30dca0b119afafa2d5d98b164da7aa903
nicholas harper <nicholasharper000@gmail.com>
2024-12-18 01:00:33 -0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,19 +1,22 @@
 module.exports = {
-  development: {
+  development: 
+  {
     username: process.env.DEV_DB_USERNAME,
     password: process.env.DEV_DB_PASSWORD,
     database: process.env.DEV_DB_NAME,
     host: process.env.DEV_DB_HOSTNAME,
     dialect: 'mysql'
   },
-  test: {
+  test: 
+  {
     username: process.env.CI_DB_USERNAME,
     password: process.env.CI_DB_PASSWORD,
     database: process.env.CI_DB_NAME,
     host: '127.0.0.1',
     dialect: 'mysql'
   },
-  production: {
+  production: 
+  {
     username: process.env.PROD_DB_USERNAME,
     password: process.env.PROD_DB_PASSWORD,
     database: process.env.PROD_DB_NAME,
