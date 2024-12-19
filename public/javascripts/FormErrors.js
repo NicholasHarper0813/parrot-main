f09286eb9c7ce129795914197d931fa5c9a23642
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 05:21:57 -0800

    Update FormErrors.js

--- public/javascripts/FormErrors.js
+++ public/javascripts/FormErrors.js
@@ -1,12 +1,14 @@
 var FormErrors = {
-    show : function ( errorList ) {
+    show : function ( errorList )
+    {
         errorList.forEach(function (error) {
             $(error.field).addClass('is-invalid');
             $('<div />').addClass('invalid-feedback').text(error.message).insertAfter(error.field);
         });
     },
 
-    clear: function () {
+    clear: function ()
+    {
         $( '.is-invalid' ).removeClass('is-invalid');
         $( '.invalid-feedback' ).remove();
     }
